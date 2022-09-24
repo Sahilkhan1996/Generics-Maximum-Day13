c8081640018625858a3a76ca7c0cea53c9f3b816
sahilkhan1996 <sahilkhan199655@gmail.com>
2022-09-24 21:10:01 +0530

    print max

--- src/com/bridgelabz/maximumproblem/MaximumProblem.java
+++ src/com/bridgelabz/maximumproblem/MaximumProblem.java
@@ -4,15 +4,15 @@ import java.util.Scanner;
 
 public class MaximumProblem {
 
-    public static void main(String[] args) {
-        GenericClass genericClass = new GenericClass();
+	public static void main(String[] args) {
+		GenericClass genericClass = new GenericClass();
 
-        System.out.println("Welcome to Maximum Problem");
+		System.out.println("Welcome to Maximum Problem");
 
-        genericClass.testMaximum(1,2,3,4,5,6,7,8,9,0);
-        genericClass.testMaximum(1.1,2.2,3.3,4.4,5.5);
-        genericClass.testMaximum("Hello","This","Is","String","Array");
+		genericClass.testMaximum(1, 2, 3, 4, 5, 6, 7, 8, 9, 0);
+		genericClass.testMaximum(1.1, 2.2, 3.3, 4.4, 5.5);
+		genericClass.testMaximum("Hello", "This", "Is", "String", "Array");
 
-    }
+	}
 
 }
