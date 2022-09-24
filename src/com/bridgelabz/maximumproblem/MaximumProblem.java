bb9924d57d5574f0b430919da6bfa8683817155e
sahilkhan1996 <sahilkhan199655@gmail.com>
2022-09-24 21:02:32 +0530

    Maximum for String numbers

--- src/com/bridgelabz/maximumproblem/MaximumProblem.java
+++ src/com/bridgelabz/maximumproblem/MaximumProblem.java
@@ -4,16 +4,17 @@ import java.util.Scanner;
 
 public class MaximumProblem {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-		System.out.println("Welcome to Maximum Problem");
+        System.out.println("Welcome to Maximum Problem");
 
-		GenericClass genericClass = new GenericClass();
+        GenericClass genericClass = new GenericClass();
 
-		System.out.println("The largest integer value is :" + genericClass.findMax(2, 9, 4));
-		System.out.println("The largest float value is :" + genericClass.findMax(0.84f, 3.78f, 9.83f));
+        System.out.println("The largest integer value is :"+genericClass.findMax(2,9,4));
+        System.out.println("The largest float value is :"+genericClass.findMax(0.84f,3.78f,9.83f));
+        System.out.println("The largest string value is :"+genericClass.findMax("Apple","Beach","Banana"));
 
-	}
+    }
 
 }
